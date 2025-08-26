3f54d5bf5556ef7c51d4141e25eee27fd1842e57
Richterskala101 <dmarend9@googlemail.com>
2025-08-26 11:27:55 +0200

    added first function and documentation

--- R/parse_datetime_and_location.R
+++ R/parse_datetime_and_location.R
@@ -1,3 +1,17 @@
+#' Parse datetime from filename (YYYYMMDD_HHMMSS or similar)
+#' @param filename A character, ususaly the name of the file, including the date and time of recording
+#' @param tz time zone (default "UTC")
+#' @return a POSIXct object/string
+#' @export
+#'
+#' @examples
+#' # example code
+#' predictions <- data.frame(
+#' filename = c("path/to/20240719_112000.WAV", "another/path/20240722_124000.WAV"))
+#' datetimes <- parse_filename_datetime(predictions)
+#' print(datetimes)
+#'
+
 parse_datetime <- function(df, filename_col = "filename") {
   lubridate::ymd_hms(basename(df[[filename_col]]))
 }
